8fced7e63f2f80258f447d393403d4f61c2cbe5c
martinalejandroo25 <martinalejandroo25@gmail.com>
2024-10-07 18:25:19 +0200

    ejercicios 17 sin terminar 18 sin terminar

---
+++ Programacion/Tema1/EjerciciosPDF/Ejercicio17.java
@@ -0,0 +1,24 @@
+package Programacion.Tema1.EjerciciosPDF;
+
+public class Ejercicio17 {
+    /*USO DE MATH.RANDOM()
+    * Para un número entre min y max
+    * double aleatorioEnRango = min + (Math.random() * (max - min))
+    * */
+    public static void main(String[] args) {
+        //a) crear un número entero positivo aleatorio entre 1 y 49
+        int a= (int)(1 + ( Math.random() * (49 - 1)));
+        System.out.println(a);
+
+        //b) Generar un número aleatorio positivo par
+        int b = 2 * ((int)(Math.random() * 50) + 1);
+        System.out.println(b);
+
+        //c) Generar un número real aleatorio entre 1 y 10
+        int c = (int)(Math.random() * 10) + 1;
+        System.out.println(c);
+        //d) Crear un número entero negativo entre -50 y -150
+        int d = (int)(Math.random() * (-150));
+        System.out.println(d);
+    }
+}
