18516ec72eecb1c7a6babc5025f65e5da3ab1c48
martinalejandroo25 <martinalejandroo25@gmail.com>
2024-11-05 15:31:11 +0100

    practicamente nada

--- Programacion/Tema2/bucles/EjerciciosPDF/Ejercicio11.java
+++ Programacion/Tema2/bucles/EjerciciosPDF/Ejercicio11.java
@@ -6,15 +6,15 @@ public class Ejercicio11 {
     // Programa (utilizando bucles) que muestre por pantalla la siguiente figura. Debe funcionar para n = número de líneas, que se pida por teclado.
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Dame el numero de filas de la piramide");
+        System.out.println("De cuantas filas va a ser la piramide?");
         int filas = Integer.parseInt(sc.nextLine());
         String ast = "*";
         String esp = " ";
-        for (int i = filas; i > 0; i++) {
+        for (int i = filas; i < 0; i--) {
+
             System.out.print(esp);
-            
+            esp += " ";
             System.out.print(ast);
-            ast += "*";
             System.out.println();
         }
     }
