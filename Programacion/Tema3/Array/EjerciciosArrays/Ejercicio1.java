abcc5ea0d34818ce9f1de5357ca89f03544204b7
martinalejandroo25 <martinalejandroo25@gmail.com>
2024-11-20 23:47:57 +0100

    ejercicio2

--- Programacion/Tema3/Array/EjerciciosArrays/Ejercicio1.java
+++ Programacion/Tema3/Array/EjerciciosArrays/Ejercicio1.java
@@ -1,10 +1,19 @@
 package Programacion.Tema3.Array.EjerciciosArrays;
 
+import java.util.Scanner;
+
 public class Ejercicio1 {
     /*1. Crea un programa que pida un número al usuario un número de mes (por ejemplo, el 4) y
 diga cuántos días tiene (por ejemplo, 30) y el nombre del mes. Debes usar un vector. Para
 simplificarlo vamos a suponer que febrero tiene 28 días.*/
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Escribe un numero del mes, 1 al 12");
+        int mes = Integer.parseInt(scanner.nextLine());
+        int[] diasPorMes = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        String[] nombreMes = {"Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"};
 
+        System.out.println("Mes: " + nombreMes[mes - 1]);
+        System.out.println("Dias: " + diasPorMes[mes - 1]);
     }
 }
---
+++ Programacion/Tema3/Array/EjerciciosArrays/Ejercicio2.java
@@ -0,0 +1,28 @@
+package Programacion.Tema3.Array.EjerciciosArrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        /*2. Programa que declare tres vectores de enteros, 10 elementos cada uno, pida por teclado los valores de vector1 y vector2 y calcule vector3 como la suma de los anteriores y lo pinte por pantalla.*/
+
+        int array1[] = new int[10];
+        int array2[] = new int[10];
+        int array3[] = new int[10];
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(" primer Array, Dame un número "+i+"/"+array1.length);
+            array1[i] = Integer.parseInt(sc.nextLine());
+        }
+        for (int i = 0; i < 10; i++) {
+            System.out.println(" Segundo Array, Dame un número "+i+"/"+array1.length);
+            array2[i] = Integer.parseInt(sc.nextLine());
+        }
+        for (int i = 0; i < array3.length; i++) {
+            array3[i] = array1[i] + array2[i];
+        }
+        System.out.println(Arrays.toString(array3));
+    }
+}
