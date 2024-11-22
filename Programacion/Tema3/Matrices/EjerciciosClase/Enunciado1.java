0f8e0c16a2c55b3b3fcbb8c8660b30fe9c2b2510
martinalejandroo25 <martinalejandroo25@gmail.com>
2024-11-22 19:03:49 +0100

    ejercicios finales html

--- Programacion/Tema3/Matrices/EjerciciosClase/Enunciado1.java
+++ Programacion/Tema3/Matrices/EjerciciosClase/Enunciado1.java
@@ -1,4 +1,24 @@
 package Programacion.Tema3.Matrices.EjerciciosClase;
 
 public class Enunciado1 {
+    /*1. Conecta 4
+Representa un tablero 6x7 que pueda contener los valores 0, 1 o 2.
+Realiza un metodo:
+    boolean colocarFicha(int tablero[][], int i, int j, int numJugador)
+
+    - Ese metodo devolverá true, si en (i,j) hay un 0, está vacía,
+    o si debajo no hay nada
+    - Si numJugador==1 pondrá en esa casilla un 1
+    - Si numJugador==2 pondrá en esa casilla un 2
+
+Realiza un metodo para comprobar si algún jugador ha ganado:
+    int comprobarGanador(int tablero[][])
+
+    - Para ello debe haber cuatro casillas contiguas con 1 o con 2. Puede ser
+    en horizontal, vertical o, si te animas, en diagonal.
+    - Devolverá 0, si no hay ninguna
+    - Devolverá 1, si hay cuatro 1 consecutivos
+    - Devolvera 2, si hay cuatro 2 consecutivos
+
+Intenta jugar una partida con dos jugadores, hasta que uno gane, o salir del juego*/
 }
